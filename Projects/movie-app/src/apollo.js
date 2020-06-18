d6a669cf92f4c69f959254288608afbcd9608fe3
IMHOJEONG <39ghwjd@naver.com>
2020-06-18 11:58:37 +0900

    Add Get Movie_Query & 테스트 주소 수정

--- Projects/movie-app/src/apollo.js
+++ Projects/movie-app/src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: "http://dlaghwjkd.test.sh/"
+    uri: "https://movieql.nomadcoders1.now.sh/"
 });
 
 export default client;
--- Projects/movie-app/src/routes/Home.js
+++ Projects/movie-app/src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
-
+import Movie from '../components/Movie';
 const GET_MOVIES = gql`
 {
     movies {
@@ -13,13 +13,24 @@ const GET_MOVIES = gql`
 
 
 export default () => {    
-    const { loading, error, data } = useQuery(GET_MOVIES);
+    const { loading, data } = useQuery(GET_MOVIES);
     // console.log(loading, error, data);
     // return <h1>"Home"</h1>
-    if(loading){
-        return "loading...";
-    }
-    if(data && data.movies){
-        return data.movies.map(m => <h1>{m.id}</h1>);
-    }
+    // if(loading){
+    //     return "loading...";
+    // }
+    // if(data && data.movies){
+    //     return data.movies.map(m => <h1>{m.id}</h1>);
+    // }
+    // {loading && <Loading>Loading</Loading>}
+    return (
+        <>
+        {loading && <div>Loading</div>}
+ {!loading 
+        && data.movies 
+        && 
+        data.movies.map(m => <Movie key={m.id} id={m.id}/>)}
+        </>
+    );
+   
 };
