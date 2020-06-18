56bc39627042bc67059f79ff4407bbbe6aa71afe
IMHOJEONG <39ghwjd@naver.com>
2020-06-18 14:54:23 +0900

    Add CSS to Apollo Exercise Page

--- Projects/movie-app/src/routes/Home.js
+++ Projects/movie-app/src/routes/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import Movie from '../components/Movie';
+import styled from 'styled-components';
+
 const GET_MOVIES = gql`
 {
     movies {
@@ -9,7 +11,49 @@ const GET_MOVIES = gql`
         medium_cover_image
     }
 
-}`; // backtick으로 작성해야 함 
+}`; // backtick으로 작성해야 함
+
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+
+const Movies = styled.div`
+    display:grid;
+    grid-template-columns:repeat(4, 1fr);
+    grid-gap:25px;
+    width:60%;
+    position:relative;
+    top:-50px;
+
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
 
 
 export default () => {    
@@ -24,13 +68,22 @@ export default () => {
     // }
     // {loading && <Loading>Loading</Loading>}
     return (
-        <>
-        {loading && <div>Loading</div>}
- {!loading 
-        && data.movies 
+        <Container>
+            <Header>
+                <Title>Apollo Test</Title>
+                <Subtitle>Made by Hojeong</Subtitle>
+            </Header>
+        {loading && <Loading>Loading</Loading>}
+        {!loading 
         && 
-        data.movies.map(m => <Movie key={m.id} id={m.id}/>)}
-        </>
+        data.movies 
+        && 
+        <Movies>
+            {data.movies.map(m => 
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image}/>)}
+        </Movies>
+        }
+       </Container>
     );
    
 };
