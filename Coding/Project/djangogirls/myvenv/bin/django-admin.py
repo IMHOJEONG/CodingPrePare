32118cbb6dd8118d3c6f24d3776f512ccbe122f3
IMHOJEONG <39ghwjd@naver.com>
2020-06-24 12:14:32 +0900

    add DjangoProject

--- Coding/Project/djangogirls/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/student/Documents/CodingPrePare/Coding/Project/djangogirls/myvenv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
